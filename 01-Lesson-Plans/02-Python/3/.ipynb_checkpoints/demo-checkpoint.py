3f2e32505cc5554625e0075f1301ed1f907f4361
Maurolp15 <maurolp15@hotmail.com>
2022-01-08 11:53:33 -0500

    Update 1-8

---
+++ 01-Lesson-Plans/02-Python/3/.ipynb_checkpoints/demo-checkpoint.py
@@ -0,0 +1,9 @@
+print('Hello this is a demo!')
+
+name = input("What is your name? ")
+
+print(f'Hello {name}, welcome!')
+
+investment = float(input("How much would you like to invest?"))
+
+print(f'Great! After 5 years, your investment of {investment} will be worth {investment * (1+.1)**(5/1)}')
---
+++ Markdown.md
@@ -0,0 +1,10 @@
+# Miami_bootcamp_wc
+
+# Big header
+## Medium header
+### Small header
+
+**Text with 2 asterisk** 
+* Text with 1 asterisk *
+
+![<name of picture>](<url/filepath of image>)
